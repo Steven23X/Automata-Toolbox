e70cdfc157a3a61799f8794d2749df774d9675b3
Steven23X <humastefan23@gmail.com>
2023-03-05 21:02:37 +0200

    Lab1 Update. Added documentation for the methods.

--- library.py
+++ library.py
@@ -1,30 +1,75 @@
 def get_file(file):
-    # contents of the file without comments and newline character and empty lines
+    """
+    get_file is processing a file and eliminates comments, new line characters and empty lines
+    :param file: type file
+    :return: it returns a list in which every element is a line of the file
+    """
     return [x.strip() for x in file if x[0] != '#' and len(x) > 1]
 
 
 def get_section(file):
-    # sections of the file, also case sensitive
+    """
+    get_section is processing a file and searches for [section] lines
+    :param file: type file
+    :return: it returns a list in which every element is a section of the file
+    """
     return [x.lower() for x in file if x[0] == "[" and x[len(x)-1] == ']']
 
 
-def get_sigma(d):  # verifies sigma which is the alphabet of the automaton
+def dictionary(sections, file):
+    """
+    dictionary verifies if the sections are correctly defined and creates a dictionary
+    :param sections: type list
+    :param file: type file
+    :return: it returns a dictionary where the key is the string section and
+    the value is a list formed of the lines after the section in the file
+    """
+    if len(sections) != len(set(sections)):
+        print("Equal Sections")
+    else:
+        print("Sections verified!")
+        d = {}
+        section = file[0].lower()
+        d[section] = []
+        for line in file[1:]:
+            if line[0] != "[":
+                d[section].append(line)
+            else:
+                d[line.lower()] = []
+                section = line.lower()
+    return d
+
+def get_sigma(d):
+    """
+    get_sigma is processing a dictionary and verifies if [sigma] section exists
+    :param d: type dictionary
+    :return: it returns the alphabet from the dictionary as a set
+    """
     if "[sigma]".lower() not in d.keys():
         print("Sigma wrongly defined")
         return False
-    # returns the alphabet from the dictionary as a set
     return set(d["[sigma]"])
 
 
-def get_states(d):  # verifies the states of the automaton
+def get_states(d):
+    """
+    get_states is processing a dictionary and verifies if [states] section exists
+    :param d: type dictionary
+    :return: it returns the states from the dictionary as a set
+    """
     if "[states]".lower() not in d.keys():
         print("states wrongly defined")
         return False
-    return set(d["[states]"])  # returns the states from a dictionary as a set
+    return set(d["[states]"])
 
 
-def test_start_final(d):  # function which verifies the start state and the final states
-    if len(d["[start]"]) != 1:  # there is only ONE start state
+def test_start_final(d):
+    """
+    test_start_final verifies the start state and the final states
+    :param d: type dictionary
+    :return: it returns True if start and final states are correctly defined
+    """
+    if len(d["[start]"]) != 1:
         print("Start wrongly defined (more than 1 state)")
         return False
     if d["[final]"] == []:
@@ -32,67 +77,76 @@ def test_start_final(d):  # function which verifies the start state and the fina
         return False
     states = set(get_states(d))
 
-    if set(d["[start]"]).issubset(states) == False:  # tests if the start state is a valid one
+    if set(d["[start]"]).issubset(states) == False:
         print("Start is not a subset of states")
         return False
-    if set(d["[final]"]).issubset(states) == False:  # tests if the final state is a valid one
+    if set(d["[final]"]).issubset(states) == False:
         print("Final is not a subset of states")
         return False
     print("Start and Final verified!")
     return True
 
 
-def get_start(d):  # function which tests the start
+def get_start(d):
+    """
+    get_start is processing a dictionary and verifies if [start] section exists
+    :param d: type dictionary
+    :return: it returns the start state from the dictionary as a set
+    """
     if "[start]" not in d.keys():
         print("Start wrongly defined")
         return False
-    return set(d["[start]"])  # returns the start state from the dictionary
+    return set(d["[start]"])
 
 
-def get_final(d):  # function which tests the final states
+def get_final(d):
+    """
+    get_final is processing a dictionary and verifies if [final] section exists
+    :param d: type dictionary
+    :return: it returns the final state from the dictionary as a set
+    """
     if "[final]" not in d.keys():
         print("Final states wrongly defined")
         return False
-    return set(d["[final]"])  # returns the final states from the dictionary
+    return set(d["[final]"])
 
 
-def get_delta(d):  # function which tests the automaton function delta
+def get_delta(d):
+    """
+    get_delta is processing a dictionary and verifies if [delta] section exists
+    :param d: type dictionary
+    :return: it returns a list in which every element is a line of the [delta] section
+             elem[0]=input state
+             elem[1]=letter from [sigma] section
+             elem[2]=output state
+             elem[0] X elem[1] -> elem[2]
+    """
     if "[delta]" not in d.keys():
-        print("Delta definit Gresit")
+        print("Delta wrongly defined")
         return False
-    # returns a nx3 matrix where a line is a delta input and output
-    mat = [x.split(",") for x in d["[delta]"]]
-    # first and second colums are the inputs
-    # the third column is the output
-    return mat  # also, returns the specified matrix
+    delta_matrix = [x.split(",") for x in d["[delta]"]]
+    return delta_matrix
 
 
 def test_delta(d):
-    mat = get_delta(d)  # matrix of delta
-    n = len(mat)  # lines
-    m = len(mat[0])  # columns
-    # as it says, we transpose this matrix
-    mat_transposed = [[mat[i][j] for i in range(0, n)] for j in range(0, m)]
     """
-    why transpose the matrix?
-    so that we can get easily the list of inputs/outputs that are (or should be) included in the states and sigma set as it follows:
-    Line 1<= States set
-    Line 2<= Sigma set
-    Line 3<= States set
-    because:
-    delta : States x Sigma --> States
+    test_delta verifies the [delta] section
+    :param d: type dictionary
+    :return: it returns True if [delta] section is correctly defined
     """
+    mat = get_delta(d)
+    rows = len(mat)
+    columns = len(mat[0])
+    mat_transposed = [[mat[i][j] for i in range(0, rows)] for j in range(0, columns)]
     states = mat_transposed[0]
-    # put into one list line 1 and 3, because they are states
     states.extend(mat_transposed[2])
     states = set(states)
-    alphabet = set(mat_transposed[1])  # get the alphabet (sigma)
+    alphabet = set(mat_transposed[1])
     if states.issubset(get_states(d)) == False:
         print("States of Delta wrongly defined")
         return False
     if alphabet.issubset(get_sigma(d)) == False:
         print("Alphabet of Delta wrongly defined")
         return False
-    # if the states and alphabet are valid, the functions makes it clear and returns  true, otherwise false
     print("Delta Ready")
     return True
